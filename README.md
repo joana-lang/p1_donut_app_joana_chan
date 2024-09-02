f77f1843a8e2872188dcdcb267b8f35a06604e44
joana-langg <le23080296@merida.tecnm.com>
2024-09-02 12:50:30 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# p1_donut_app_joana_chan
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/p1_donut_app_joana_chan/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.p1_donut_app_joana_chan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
